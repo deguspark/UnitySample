4bcfbcd7c2d5b7f070bfa24d49ee072d5542a282
shogonir <shogonir@gmail.com>
2017-10-15 00:17:20 +0900

    [modify] 01 11 Bullet Networking

--- 01_MultiPlayerShooting/Assets/Scripts/PlayerController.cs
+++ 01_MultiPlayerShooting/Assets/Scripts/PlayerController.cs
@@ -3,8 +3,8 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour {
     
-	public GameObject bulletPrefab;
-	public Transform bulletSpawn;
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
 
     void Update () {
         if (!isLocalPlayer)
@@ -12,17 +12,17 @@ public class PlayerController : NetworkBehaviour {
             return;
         }
 
-		// move
+        // move
         var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
         var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
         transform.Rotate(0, x, 0);
         transform.Translate(0, 0, z);
 
-		// fire
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			Fire();
-		}
+        // fire
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            CmdFire();
+        }
     }
 
     public override void OnStartLocalPlayer()
@@ -30,18 +30,21 @@ public class PlayerController : NetworkBehaviour {
         GetComponent<MeshRenderer>().material.color = Color.blue;
     }
 
-	void Fire()
-	{
-		// Bullet プレハブから Bullet を生成する
-		var bullet = (GameObject)Instantiate (
-			bulletPrefab,
-			bulletSpawn.position,
-			bulletSpawn.rotation);
+    [Command]
+    void CmdFire()
+    {
+        // Bullet プレハブから Bullet を生成する
+        var bullet = (GameObject)Instantiate (
+            bulletPrefab,
+            bulletSpawn.position,
+            bulletSpawn.rotation);
 
-		// 弾の速度を増加させる
-		bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        // 弾の速度を増加させる
+        bullet.GetComponent<Rigidbody>().velocity = bullet.transform.forward * 6;
+        
+        NetworkServer.Spawn(bullet);
 
-		// 2 秒後に弾を破壊する
-		Destroy(bullet, 2.0f);
-	}
+        // 2 秒後に弾を破壊する
+        Destroy(bullet, 2.0f);
+    }
 }
